8e214788b834bbd13ed72d7ee9318cbbf902191e
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-01 00:38:10 +0600

    Added json web token

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,22 @@
-import { Router } from "express";
+import {  Router } from "express";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
 
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuths";
+import { Role } from "./user.interface";
+// import  { JwtPayload } from "jsonwebtoken";
+// import AppError from "../../errorHelpers/AppError";
+// // import { Role } from "./user.interface";
+
+// import { envVars } from "../../config/env";
+// import { verifyToken } from "../../utils/jwt";
 
 const router = Router();
+
+
+
+
 
 router.post(
   "/register",
@@ -12,6 +24,11 @@ router.post(
   UserControllers.createUser
 );
 
-router.get("/all-users", UserControllers.getAllUsers);
+
+
+router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+ UserControllers.getAllUsers);
+
+ router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
 
 export const userRoutes = router;
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
+import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
 let server: Server;
 //
@@ -20,7 +21,10 @@ const startServer = async () => {
   }
 };
 
-startServer();
+(async () => {
+  await startServer();
+  await seedSuperAdmin();
+})();
 
 // unhandled rejection error
 // uncaught rejection error
