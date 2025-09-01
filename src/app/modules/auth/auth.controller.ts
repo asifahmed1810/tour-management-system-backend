c62eb710f78a82fa9b77c9b1e95eb6841a34e030
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-01 22:03:18 +0600

    Added update password feature

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,71 +1,98 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
-import httpStatus from "http-status-codes"
+import httpStatus from "http-status-codes";
 import { sendResponse } from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
 import { setAuthCookie } from "../../utils/setCookie";
+import { JwtPayload } from "jsonwebtoken";
 
-const credentialsLogin=catchAsync(
+const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-   
-
-    const loginInfo=await AuthServices.credentialsLogin(req.body);
-
+    const loginInfo = await AuthServices.credentialsLogin(req.body);
 
     //  res.cookie("accessToken",loginInfo.accessToken,{
     //   httpOnly:true,
     //   secure:false,
     // })
 
-   
-
-
     // res.cookie("refreshToken",loginInfo.refreshToken,{
     //   httpOnly:true,
     //   secure:false,
     // })
 
+    setAuthCookie(res, loginInfo);
 
-     setAuthCookie(res,loginInfo)
-
-
-    sendResponse(res,{
-        success:true,
-        statusCode:httpStatus.OK,
-        message:"User Logged in Successfully",
-        data:loginInfo
-    })
-
-
-
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged in Successfully",
+      data: loginInfo,
+    });
   }
 );
 
-const getNewAccessToken=catchAsync(
+const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-   
-    const refreshToken=req.cookies.refreshToken;
+    const refreshToken = req.cookies.refreshToken;
 
-    const tokenInfo=await AuthServices.getNewAccessToken(refreshToken as string)
+    const tokenInfo = await AuthServices.getNewAccessToken(
+      refreshToken as string
+    );
 
-   
-  setAuthCookie(res,tokenInfo)
+    setAuthCookie(res, tokenInfo);
 
-
-    sendResponse(res,{
-        success:true,
-        statusCode:httpStatus.OK,
-        message:"User Logged in Successfully",
-        data:tokenInfo
-    })
-
-
-
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "New access token retrived  Successfully",
+      data: tokenInfo,
+    });
   }
 );
 
-export const  AuthControllers={
-    credentialsLogin,
-    getNewAccessToken
-}
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Logged out  Successfully",
+      data: null,
+    });
+  }
+);
+
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user
+
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken as JwtPayload);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password Changed Successfully",
+        data: null,
+    })
+})
+
+export const AuthControllers = {
+  credentialsLogin,
+  getNewAccessToken,
+  logout,
+  resetPassword,
+};
