c93e77abcc14a505a1e083045c41fd89d78d6450
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-04 22:40:29 +0600

    Update booking folder

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -18,7 +18,12 @@ interface EnvConfig {
   GOOGLE_CALLBACK_URL: string;
   EXPRESS_SESSION_SECRET: string;
   FRONTEND_URL: string;
-
+  SSL: {
+    STORE_ID: string;
+    STORE_PASS: string;
+    SSL_PAYMENT_API: string;
+    SSL_VALIDATION_API: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -37,7 +42,11 @@ const loadEnvVariables = (): EnvConfig => {
     "GOOGLE_CLIENT_ID",
     "GOOGLE_CALLBACK_URL",
     "EXPRESS_SESSION_SECRET",
-    "FRONTEND_URL"
+    "FRONTEND_URL",
+    "SSL_STORE_ID",
+    "SSL_STORE_PASS",
+    "SSL_PAYMENT_API",
+    "SSL_VALIDATION_API",
   ];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -52,19 +61,21 @@ const loadEnvVariables = (): EnvConfig => {
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
-    JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
-    JWT_REFRESH_EXPIRES:process.env.JWT_REFRESH_EXPIRES as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
-    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET as string,
-    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID as string,
-    GOOGLE_CALLBACK_URL:process.env. GOOGLE_CALLBACK_URL as string,
-    EXPRESS_SESSION_SECRET:process.env. EXPRESS_SESSION_SECRET as string,
-    FRONTEND_URL:process.env. FRONTEND_URL as string
-
-
-
-
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+     SSL: {
+            STORE_ID: process.env.SSL_STORE_ID as string,
+            STORE_PASS: process.env.SSL_STORE_PASS as string,
+            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+        },
   };
 };
 
