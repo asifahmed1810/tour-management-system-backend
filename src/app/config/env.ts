fac42d41ca66bbf4800d68bb0243333d66f88483
asifahmed1810 <asifahmed18oct@gmail.com>
2025-09-04 22:42:48 +0600

    Update env.ts file

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -23,6 +23,12 @@ interface EnvConfig {
     STORE_PASS: string;
     SSL_PAYMENT_API: string;
     SSL_VALIDATION_API: string;
+    SSL_SUCCESS_FRONTEND_URL: string;
+    SSL_FAIL_FRONTEND_URL: string;
+    SSL_CANCEL_FRONTEND_URL: string;
+    SSL_SUCCESS_BACKEND_URL: string;
+    SSL_FAIL_BACKEND_URL: string;
+    SSL_CANCEL_BACKEND_URL: string;
   };
 }
 
@@ -47,6 +53,12 @@ const loadEnvVariables = (): EnvConfig => {
     "SSL_STORE_PASS",
     "SSL_PAYMENT_API",
     "SSL_VALIDATION_API",
+    "SSL_SUCCESS_FRONTEND_URL",
+    "SSL_FAIL_FRONTEND_URL",
+    "SSL_CANCEL_FRONTEND_URL",
+    "SSL_SUCCESS_BACKEND_URL",
+    "SSL_FAIL_BACKEND_URL",
+    "SSL_CANCEL_BACKEND_URL",
   ];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -70,12 +82,18 @@ const loadEnvVariables = (): EnvConfig => {
     GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
     EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
     FRONTEND_URL: process.env.FRONTEND_URL as string,
-     SSL: {
-            STORE_ID: process.env.SSL_STORE_ID as string,
-            STORE_PASS: process.env.SSL_STORE_PASS as string,
-            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
-            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
-        },
+    SSL: {
+      STORE_ID: process.env.SSL_STORE_ID as string,
+      STORE_PASS: process.env.SSL_STORE_PASS as string,
+      SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+      SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+      SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+      SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+      SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+      SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+      SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+      SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
+    },
   };
 };
 
